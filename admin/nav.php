8edf4fefffc3a783b824a61571853e88ec989526
parham trojan <parhamtrojan@hotmail.com>
2020-10-30 10:36:25 +0330

    admin panel
    This update includes the admin panel

---
+++ admin/nav.php
@@ -0,0 +1,99 @@
+<nav class="w3-sidebar w3-collapse w3-white w3-animate-right w3-right" style="z-index:3;width:300px;" id="mySidebar">
+    <br>
+    <div class="w3-container w3-row">
+        <div class="w3-col s8 w3-bar" style="text-align: center;">
+            <a href="#" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i></a>
+            <a href="#" class="w3-bar-item w3-button"><i class="fa fa-user"></i></a>
+            <a href="#" class="w3-bar-item w3-button"><i class="fa fa-cog"></i></a>
+        </div>
+    </div>
+    <hr>
+    <div class="w3-container w3-blue" style=" padding: 10px;">
+        <b style="font-size: 14px;">داشبورد</b>
+    </div>
+    <div class="w3-bar-block  w3-right-align">
+        <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black w3-right-align"
+           onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>  بستن</a>
+        <hr>
+        <b class="w3-margin">اعلان</b>
+        <a href="news.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-eye fa-fw"></i>
+            خبر</a>
+        <hr>
+        <b class="w3-margin">کاربران</b>
+        <a href="admin_user.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-users fa-fw"></i> 
+            مدیران ارشد</a>
+        <a href="user_group.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-users fa-fw"></i> 
+            گروه کاربران</a>
+        <a href="user_game.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-users fa-fw"></i> 
+            کاربران</a>
+        <a href="fast_user_game.php" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-users fa-fw"></i> 
+            ثبت کاربر سریع</a>
+        <hr>
+        <b class="w3-margin">سیستم ها</b>
+        <a href="computer_group.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-eye fa-fw"></i>
+            گروه کامپیوترها</a>
+        <a href="computer_game.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-users fa-fw"></i> 
+            کامپیوترها</a>
+        <hr>
+
+
+        <b class="w3-margin">مدیریت سطح دسترسی</b>
+        <a href="acc_group.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-eye fa-fw"></i>
+            گروه های سطح دسترسی</a>
+        <a href="access.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-eye fa-fw"></i>
+            سطح دسترسی</a>
+
+        <hr>
+
+        <b class="w3-margin">حساب و کتاب</b>
+        <a href="acount.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-bullseye fa-fw"></i> 
+            حسابها</a>
+        <a href="pay_for.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-bullseye fa-fw"></i> 
+            بابت سریع</a>
+        <a href="payment.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-bank fa-fw"></i> 
+            پرداخت و دریافت ها</a>
+        <a href="fast_pay.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-bank fa-fw"></i> 
+            واریز سریع</a>
+        <a href="fast_get.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-bank fa-fw"></i> 
+            برداشت سریع</a>
+        <hr>
+        <b class="w3-margin">مدیریت بازی و نرم افزار</b>
+        <a href="soft_cat.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-bullseye fa-fw"></i> 
+            دسته بندی ها</a>
+        <a href="computer_soft.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-bank fa-fw"></i> 
+            بازی ها</a>
+        <a href="soft.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-bank fa-fw"></i> 
+            نرم افزارها</a>
+        <a href="settings.php?action=show" class="w3-bar-item w3-button w3-padding w3-right-align"><i
+                    class="fa fa-bank fa-fw"></i> 
+            ابزارها</a>
+        <hr>
+
+        <a href="#" class="w3-bar-item w3-button w3-padding w3-right-align"><i class="fa fa-diamond fa-fw"></i>  Orders</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding w3-right-align"><i class="fa fa-bell fa-fw"></i>  News</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding w3-right-align"><i class="fa fa-bank fa-fw"></i> 
+            General</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding w3-right-align"><i class="fa fa-history fa-fw"></i>  History</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding w3-right-align"><i class="fa fa-cog fa-fw"></i> 
+            Settings</a>
+        <br><br>
+        <br><br>
+    </div>
+</nav>
