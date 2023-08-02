538cdd78983173bd5aa5d8fcd21ae201c172e525
parham ahmadipeymah <46413141+parhampa@users.noreply.github.com>
2023-08-02 20:26:18 +0330

    Add files via upload
    make better user interface

--- source/admin/login.php
+++ source/admin/login.php
@@ -5,6 +5,7 @@
  * Date: 8/25/2020
  * Time: 4:01 PM
  */
+session_start();
 include("../lib/php/lib_include.php");
 ?>
 <!DOCTYPE html>
@@ -25,13 +26,37 @@ include("../lib/php/lib_include.php");
         font-size: 12px;
     }
 </style>
-<body class="w3-light-grey" style="direction: rtl;">
-<div style="width: 40%; margin-right: 30%; margin-top: 150px; background-color: white; padding: 70px;">
-    <h1>ورود به پنل مدیریت</h1>
-    <?php
-    $lg = new loginpg();
-    $lg->showlogin("admin_user", "username", "pass", "main.php");
-    ?>
+<body class="w3-light-grey" style="direction: rtl;" onload="scch()">
+<div style="width: 60%; margin-right: 20%; margin-top: 10px; background-color: white; padding: 70px;" class="w3-card-2"
+     id="fdive">
+    <br>
+    <br>
+    <br>
+    <div style="width: 60%;margin-right: 20%;" id="sdive">
+        <h3>ورود به پنل مدیریت</h3>
+        <?php
+        $lg = new loginpg();
+        $lg->showlogin("admin_user", "username", "pass", "index.php");
+        ?>
+    </div>
+
+    <br>
+    <h5>
+        طراحی و پیاده سازی
+        <span class="w3-text-green">
+            تیم نرم افزاری X4Y
+	</span>
+    </h5>
 </div>
+<script>
+    function scch() {
+        if (window.screen.availWidth <= 600) {
+            document.getElementById('fdive').style.width = "90%";
+            document.getElementById('fdive').style.marginRight = "5%";
+            document.getElementById('sdive').style.width = "100%";
+            document.getElementById('sdive').style.marginRight = "0px";
+        }
+    }
+</script>
 </body>
 </html>
