e310288877c5d66f7476115defa99fcc64bdbc94
parham ahmadipeymah <46413141+parhampa@users.noreply.github.com>
2023-08-31 03:45:57 +0330

    We have a new type of modal

--- source/lib/php/modal.php
+++ source/lib/php/modal.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: ormazd
@@ -18,6 +19,24 @@ class modal
         $res .= "</div></div></div>";
         echo($res);
     }
+
+    function add_inner_modal_top($id)
+    {
+        $res = "<!-- The Modal -->";
+        $res .= '<div id="' . $id . '" class="w3-modal">';
+        $res .= '<div class="w3-modal-content">';
+        $res .= '<div class="w3-container" style="padding: 50px;">';
+        $res .= '<span onclick="document.getElementById(' . "'" . $id . "'" . ').style.display=' . "'" . 'none' . "'" . '" class="w3-button w3-display-topright">&times;</span>';
+        echo($res);
+    }
+
+    function add_inner_modal_down()
+    {
+        $res = "</div></div></div>";
+        echo($res);
+    }
+
+
 }
 
 ?>
